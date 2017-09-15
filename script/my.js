dd305ed58447f540ef2aa490fadb4ef7e95ee608
1007237083@qq.com <1007237083@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2017-09-15 02:23:38 +0000

    1
    git-svn-id: svn://svn5.apicloud.com/A6058908802893/%E5%AD%A6%E4%BB%A4%E7%89%8CAndroid@12 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/my.js
+++ script/my.js
@@ -10,3 +10,37 @@ function rem_fontSize(hRemVal){
     numVal = hRemVal * htmlUnitFontSize / 2;
     return numVal;
 }
+
+//IOS设置bar
+function fixIos7Bar_API(el){
+    if(!$api.isElement(el)){
+        console.warn('$api.fixIos7Bar Function need el param, el param must be DOM Element');
+        return;
+    }
+    var strDM = $api.getStorage('SYSTEMTYPE');
+    if (strDM == 'ios') {
+        var strSV = $api.getStorage('SYSTEMVERSION');
+        var numSV = parseInt(strSV,10);
+        var fullScreen = $api.getStorage('FULLSCREEN');
+        var iOS7StatusBarAppearance = $api.getStorage('IOS7STATUSBARAPPEARANCE');
+        if (numSV >= 7 && fullScreen == 'false' && iOS7StatusBarAppearance) {
+            el.style.paddingTop = '20px';
+        }
+    }
+}
+function fixStatusBar_API(el){
+    if(!$api.isElement(el)){
+        console.warn('$api.fixStatusBar Function need el param, el param must be DOM Element');
+        return;
+    }
+    var sysType = $api.getStorage('SYSTEMTYPE');
+    if(sysType == 'ios'){
+        fixIos7Bar_API(el);
+    }else if(sysType == 'android'){
+        var ver = $api.getStorage('SYSTEMVERSION');
+        ver = parseFloat(ver);
+        if(ver >= 4.4){
+            el.style.paddingTop = '25px';
+        }
+    }
+};
