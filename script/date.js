8d28ed91efce8d08cf0ab772e41fb694f1272d41
1007237083@qq.com <1007237083@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-01-06 08:09:32 +0000

    1-6
    git-svn-id: svn://svn5.apicloud.com/A6058908802893/%E5%AD%A6%E4%BB%A4%E7%89%8CAndroid@106 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/date.js
+++ script/date.js
@@ -1,31 +1,34 @@
+
 // vue时间戳转换
-function formatDate (date, fmt) {
+function formatDate(date, fmt) {
     "use strict";
+
     if (/(y+)/.test(fmt)) {
         fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
-    let o = {
+    var o = {
         'M+': date.getMonth() + 1,
         'd+': date.getDate(),
         'h+': date.getHours(),
         'm+': date.getMinutes(),
         's+': date.getSeconds()
     };
-    for (let k in o) {
-        if (new RegExp(`(${k})`).test(fmt)) {
-            let str = o[k] + '';
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+    for (var k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            var str = o[k] + '';
+            fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? str : padLeftZero(str));
         }
     }
     return fmt;
 };
 
-function padLeftZero (str) {
+function padLeftZero(str) {
     return ('00' + str).substr(str.length);
 };
 
 //时间戳转换
-Date.prototype.format = function(fmt){ //author: meizz
+Date.prototype.format = function (fmt) {
+    //author: meizz
     var o = {
         "M+": this.getMonth() + 1, //月份
         "d+": this.getDate(), //日
@@ -36,28 +39,27 @@ Date.prototype.format = function(fmt){ //author: meizz
         "S": this.getMilliseconds() //毫秒
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt))
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+    }return fmt;
+};
+
+function Gtime_mdhm(data_) {
+    time = new Date(parseInt(data_) * 1000).format('M月d日 hh:mm');
+    return time;
 }
 
-function Gtime_mdhm(data_){
-	time=new Date(parseInt(data_) * 1000).format('M月d日 hh:mm');
-	return time;
+function Gtime_md(data_) {
+    time = new Date(parseInt(data_) * 1000).format('M月d日');
+    return time;
 }
 
-function Gtime_md(data_){
-	time=new Date(parseInt(data_) * 1000).format('M月d日');
-	return time;
+function Gtime_ymd(data_) {
+    time = new Date(parseInt(data_) * 1000).format('yyyy年M月d日');
+    return time;
 }
 
-function Gtime_ymd(data_){
-	time=new Date(parseInt(data_) * 1000).format('yyyy年M月d日');
-	return time;
-}
-
-function getDate(time){
+function getDate(time) {
     var str = time;
     //将字符串转换成时间格式
     var timePublish = new Date(str * 1000);
@@ -74,33 +76,24 @@ function getDate(time){
     var diffMinute = diffValue / minute;
 
     if (diffValue < 0) {
-       console.log("错误时间");
-    }
-    else if (diffMonth > 3) {
-        result = timePublish.getFullYear()+"-";
-        result += (timePublish.getMonth() + 1) + "-";
+        console.log("错误时间");
+    } else if (diffMonth > 3) {
+        result = timePublish.getFullYear() + "-";
+        result += timePublish.getMonth() + 1 + "-";
         result += timePublish.getDate();
-
-    }
-    else if (diffMonth > 1) {
+    } else if (diffMonth > 1) {
         result = parseInt(diffMonth) + "月前";
-    }
-    else if (diffWeek > 1) {
+    } else if (diffWeek > 1) {
         result = parseInt(diffWeek) + "周前";
-    }
-    else if (diffDay > 1) {
+    } else if (diffDay > 1) {
         result = parseInt(diffDay) + "天前";
-    }
-    else if (diffHour > 1) {
+    } else if (diffHour > 1) {
         result = parseInt(diffHour) + "小时前";
-    }
-    else if (diffMinute > 1) {
+    } else if (diffMinute > 1) {
         result = parseInt(diffMinute) + "分钟前";
-    }
-    else {
+    } else {
         result = "刚刚";
     }
 
     return result;
-
 }
