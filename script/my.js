05d07d81ed4acc5a026355185997ea16fc78d848
1007237083@qq.com <1007237083@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-04-01 09:56:12 +0000

    4-1
    git-svn-id: svn://svn5.apicloud.com/A6058908802893/%E5%AD%A6%E4%BB%A4%E7%89%8CAndroid@133 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/my.js
+++ script/my.js
@@ -14,182 +14,40 @@ function go_back() {
     });
 }
 
+// 状态栏设置
+function SetStatusBar(el,style,color){
+    //适配iOS7+、Android4.4+系统状态栏，为传入的DOM元素增加适当的上内边距，避免header与状态栏重叠
+    // $api.fixStatusBar( $api.dom(el) );
 
-//IOS设置bar
-function fixIos7Bar_API(el) {
-    if (!$api.isElement(el)) {
-        console.warn('$api.fixIos7Bar Function need el param, el param must be DOM Element');
-        return;
-    }
-    var strDM = $api.getStorage('SYSTEMTYPE');
-    if (strDM == 'ios') {
-        var strSV = $api.getStorage('SYSTEMVERSION');
-        var numSV = parseInt(strSV, 10);
-        var fullScreen = $api.getStorage('FULLSCREEN');
-        var iOS7StatusBarAppearance = $api.getStorage('IOS7STATUSBARAPPEARANCE');
-        if (numSV >= 7 && fullScreen == 'false' && iOS7StatusBarAppearance) {
-            el.style.paddingTop = '20px';
-        }
-    }
-}
-
-function fixStatusBar_API(el) {
-    if (!$api.isElement(el)) {
-        console.warn('$api.fixStatusBar Function need el param, el param must be DOM Element');
-        return;
-    }
-    var sysType = $api.getStorage('SYSTEMTYPE');
-    if (sysType == 'ios') {
-        fixIos7Bar_API(el);
-    } else if (sysType == 'android') {
-        var ver = $api.getStorage('SYSTEMVERSION');
-        ver = parseFloat(ver);
-        if (ver >= 4.4) {
-            el.style.paddingTop = '25px';
-        }
-    }
-};
-
-
-//加载 XML 文档函数
-function parseXML(ret) {
-    var xmlDoc = null;
-    try //Internet Explorer
-    {
-        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async = "false";
-        xmlDoc.loadXML(ret);
-    } catch (e) {
-        try //Firefox, Mozilla, Opera, etc.
-        {
-            parser = new DOMParser();
-            xmlDoc = parser.parseFromString(ret, "text/xml");
-        } catch (e) {
-            alert(e.message)
-        }
-    }
-    return xmlDoc;
-}
-
-//加载loading
-function loading() {
-    var str = '';
-    str += '<div class="load-container load8" id="loading">' +
-        '<div class="loader"></div>' +
-        '<div class="txt">加载中</div>' +
-        '</div>'
-    $api.append($api.dom('body'), str);
-}
-
-// 发送验证码倒计时
-function SendCountDown(obj, wait) {
-    if (wait == 0) {
-        obj.removeAttribute("disabled");
-        obj.value = "获取验证码";
-        wait = 60;
-        return;
-    } else {
-        obj.setAttribute("disabled", true);
-        obj.value = "重新发送(" + wait + ")";
-        wait--;
-    }
-    setTimeout(function() {
-        SendCountDown(obj, wait);
-    }, 1000)
-}
-
-// 发送手机验证码
-function SendCode(obj, tel, wait) {
-
-    api.ajax({
-        url: serverurl,
-        method: 'get',
-        data: {
-            values: {
-                type: '10042',
-                phone: tel
-            },
-        }
-    }, function(ret, err) {
-
-        switch (ret.status) {
-            case '1':
-                api.toast({
-                    msg: '验证码已发送，请留意短信',
-                    duration: 2000,
-                    location: 'bottom'
-                });
-                SendCountDown(obj, wait);
-                break;
-            case '2':
-                api.toast({
-                    msg: '一个小时只能发送5条',
-                    duration: 2000,
-                    location: 'bottom'
-                });
-                break;
-            case '3':
-                api.toast({
-                    msg: '请间隔60s后重新获取',
-                    duration: 2000,
-                    location: 'bottom'
-                });
-                break;
-            case '4':
-                api.toast({
-                    msg: '手机号已经注册',
-                    duration: 2000,
-                    location: 'bottom'
-                });
-                break;
-            case '5':
-              api.toast({
-                  msg: '超过一天接收数量',
-                  duration: 2000,
-                  location: 'bottom'
-              });
-              break;
-        }
+    //设置状态栏样式为白色（适用于深色背景）或黑色（适用于浅色背景），以及设置状态栏背景颜色
+    api.setStatusBarStyle({
+        style: style,
+        color: color
     });
-
 }
 
-
-/**
-*小数转换为百分比
-*这里需要先用Number进行数据类型转换，然后去指定截取转换后的小数点后几位(按照四舍五入)，这里是截取一位，0.1266转换后会变成12.7%
-*/
-function toPercent(point){
-    var str=Number(point*100);
-    str+="%";
-    return str;
-}
-
-
-//解决键盘弹出底部导航被顶上来的bug
-function focus_footer(){
-
-    var h = document.body.scrollHeight;
-    window.onresize = function() {
-      if (document.body.scrollHeight < h) {
-        document.getElementById('footer').style.display = "none";
-      } else {
-        document.getElementById('footer').style.display = "block";
-      }
-    };
-}
-
-//用js限制字数，超出部分以省略号...显示
-function LimitNumber(txt,idName) {
-    var str = txt;
-    str = str.substr(0,20) + '...' ;
-    var id=document.getElementById(idName);
-    id.innerText=str;
-}
 
 // 退出登录
 function ExitLogin(){
   $api.clearStorage();//清除localStorage的所有数据，慎用
+  api.removePrefs({
+    key: 'token'
+  });
+  api.removePrefs({
+    key: 'id'
+  });
+  api.removePrefs({
+    key: 'user_name'
+  });
+  api.removePrefs({
+    key: 'headimgurl'
+  });
+  api.removePrefs({
+    key: 'registrationId'
+  });
+  api.setFullScreen({
+    fullScreen: true
+  });
   setTimeout(function(){
     api.closeToWin({
         name: 'root'
@@ -199,3 +57,108 @@ function ExitLogin(){
 
 // 去掉首尾空格
 function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+
+//解决键盘弹出底部导航被顶上来的bug
+function focus_footer(){
+    var h = document.body.scrollHeight;
+
+    window.onresize = function() {
+
+      if (document.body.scrollHeight < h) {
+
+        document.getElementById('footer').style.display = "none";
+      } else {
+        document.getElementById('footer').style.display = "block";
+      }
+    };
+}
+
+// 封装api.openFrame
+function getFrame(name,url,rect,pageParam){
+    api.openFrame({
+        name: name,
+        url: url,
+        bounces: false,
+        bgColor: '#f2f2f2',
+        vScrollBarEnabled: false,
+        hScrollBarEnabled: false,
+        softInputMode:'resize',
+        rect:rect,
+        pageParam: pageParam,
+        reload: true,
+        allowEdit:true,
+    });
+}
+
+// 封装api.openWin
+function openWin(name,url,pageParam){
+    api.openWin({
+        name: name,
+        url: url,
+        pageParam: pageParam,
+        scrollToTop:true,//当点击状态栏，页面是否滚动到顶部,只 iOS 有效
+        slidBackEnabled:false,//（可选项）是否支持滑动返回。iOS7.0及以上系统中，在新打开的页面中向右滑动，可以返回到上一个页面，该字段只 iOS 有效
+        animation:{
+          type:"fade",                //动画类型（详见动画类型常量）
+          subType:"from_bottom",       //动画子类型（详见动画子类型常量）
+          duration:100
+        },
+        progress:{//（可选项）页面加载进度配置信息，若不传则无加载进度效果
+            color:'#45C01A'
+        },
+        reload: true,
+        allowEdit:true//（可选项）是否允许长按页面时弹出选择菜单
+
+    });
+
+}
+
+//封装toast提示框
+function msg(msg){
+    api.toast({
+        msg: msg,
+        duration: 3000,
+        location: 'middle'
+    });
+}
+
+// 封装加载进度条
+function showProgress(title,text){
+    api.showProgress({
+      title: title,
+      text: text,
+      modal: false
+    });
+}
+
+/**
+ *方法：Array.del(index)
+ *功能：删除数组的元素
+**/
+Array.prototype.del=function(n) {　//n表示第几项，从0开始算起。
+//prototype为对象原型，注意这里为对象增加自定义方法的方法。
+　if(n<0)　//如果n<0，则不进行任何操作。
+  return this;
+  　else
+  return this.slice(0,n).concat(this.slice(n+1,this.length));
+  /*
+  　concat方法：返回一个新数组，这个新数组是由两个或更多数组组合而成的。
+  　这里就是返回this.slice(0,n)/this.slice(n+1,this.length)
+   组成的新数组，这中间，刚好少了第n项。
+  　slice方法： 返回一个数组的一段，两个参数，分别指定开始和结束的位置。
+  */
+}
+
+/*
+   *方法：AddorRemove(type,el);type:add添加，remove移除、el：dom元素
+   *功能：实时监测input框移除button disabled元素，改编按钮颜色状态
+*/
+function AddorRemove(type,el){
+    if (type == 'add') {
+        $api.addCls($api.dom(el), 'on');
+        $api.removeAttr($api.dom(el), 'disabled');
+    }else if(type == 'remove'){
+        $api.removeCls($api.dom(el), 'on');
+        $api.attr($api.dom(el), 'disabled');
+    }
+}
